adba74fe55242a74c66139cc3a5b19c23f5b71e1
Andre <andre.segovia@utec.edu.pe>
2019-08-28 13:31:17 -0500

    Empezado radixsort

--- algorithms.h
+++ algorithms.h
@@ -6,12 +6,15 @@ template<typename T>
 class Sorting {
   void swap(T* a, T* b);
   int partir(T* arreglo, int front, int back);
+  int max(T* arreglo, int size);
+  void csort(T* arreglo, int size, int n);
   public:
   void imparray(T* arreglo, int size);
   //void insertion_sort(T* arreglo, int size);
   void bubble_sort(T* arreglo, int size);
   //void selection_sort(T* arreglo, int size);
   void quick_sort(T* arreglo, int front, int back);
+  void radix_sort(T* arreglo, int size);
 };
 
 template<typename T>
@@ -30,5 +33,19 @@ void Sorting<T>::imparray(T* arreglo, int size)
     std::cout << arreglo[i] << " ";
   }
   std::cout << std::endl;
+}
+
+template<typename T>
+int Sorting<T>::max(T* arreglo, int size)
+{
+  int max = arreglo[0];
+  for(int i = 1; i < size; i++)
+  {
+    if(arreglo[i] > max)
+    {
+      max = arreglo[i];
+    }
+  }
+  return max;
 }
 #endif
---
+++ radixsort.cpp
@@ -0,0 +1,13 @@
+#include "./algorithms.h"
+
+template<typename T>
+void Sorting<T>::radix_sort(T* arreglo, int size)
+{
+  
+}
+
+template<typename T>
+void Sorting<T>::csort(T* arreglo, int size, int n)
+{
+
+}
