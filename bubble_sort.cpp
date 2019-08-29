3daad55c2bd8680197687802dd384d181dc40b18
Os <os@osmancampeon69@gmail.com>
2019-08-28 22:54:28 -0500

    bubble sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,7 +1,28 @@
 #include "./algorithms.h"
 
-template<typename T>
-void Sorting<T>::bubble_sort(T* arreglo, int size)
+vector<T> Sorting<T>::bubble_sort(vector<T> arreglo)
 {
-  int a = 5;
+    int n = arreglo.size();
+    vector<T> arr;
+    for(int i = 0;i<n;i++){
+        arr.push_back(arreglo[i]);
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < n-1; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                int temp1 = arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp1;
+            }
+            
+        }
+    }
+    for (int k = 0; k < n; k++)
+    {
+        cout<<arr[k]<<" ";
+    }
+    return arr;
 }
