b4ff5fe4a3967b5ecbfe30c8c6405feb1714b243
Apple <apple@Apples-MacBook-Pro.local>
2019-08-28 19:29:17 -0500

    MI SELECTION SORT UWU

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include "./bubble_sort.cpp"
 #include "./quicksort.cpp"
 #include "./radixsort.cpp"
+#include "./selectionsort.cpp"
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
   Sorting<int> a;
   int arreglo[] = {8, 3, 6, 4, 2, 5, 7, 1};
-  a.radix_sort(arreglo, 8);
+  a.selection_sort(arreglo, 8);
   a.imparray(arreglo, 8);
   return 0;
 }
