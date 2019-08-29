1d77e5f4e8d83a116048afc0d48288f64faae57e
Apple <apple@Apples-MacBook-Pro.local>
2019-08-28 19:52:41 -0500

    heapsort

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 #include "./quicksort.cpp"
 #include "./radixsort.cpp"
 #include "./selectionsort.cpp"
+#include "./heapsort.cpp"
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
   Sorting<int> a;
   int arreglo[] = {8, 3, 6, 4, 2, 5, 7, 1};
-  a.selection_sort(arreglo, 8);
+  a.heapsort(arreglo, 8);
   a.imparray(arreglo, 8);
   return 0;
 }
